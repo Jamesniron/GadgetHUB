6222a39ffd515709850b6c55a614630bb87c10b1
Nithushan UCA <nithshan@unicornconnectedapps.com>
2025-07-24 14:06:56 +0530

    git ignore

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6ea569abf91394d41fff8845735ec2fbc49eb5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab8b095a8b035846622300e0e3c2760d691e0d48")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

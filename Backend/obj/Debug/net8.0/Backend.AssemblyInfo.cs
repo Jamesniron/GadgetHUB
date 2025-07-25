be05e80fae92a2ae8ae5a7739a4226715a55650a
Nithushan UCA <nithshan@unicornconnectedapps.com>
2025-07-25 14:04:40 +0530

    data Base Checked

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcaa6a36d12af5111f5a56b318abf1ed867c8591")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56440326bc5f5da5baa663d7b2ce04e591194bb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

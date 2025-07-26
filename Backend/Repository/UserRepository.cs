d4c8f6b19c953168e4a34676a3150d6e7ceb0527
Nithushan UCA <nithshan@unicornconnectedapps.com>
2025-07-26 21:42:33 +0530

    added user entity and login api's

---
+++ Backend/Repository/UserRepository.cs
@@ -0,0 +1,48 @@
+﻿using Backend.Dbcontext;
+using Backend.Entity;
+using Backend.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Repository
+{
+  public class UserRepository : IUserRepository
+  {
+    private readonly AppSetting _DbContext;
+    public UserRepository(AppSetting appSetting)
+    {
+      _DbContext = appSetting;
+    }
+
+    //Create Account
+    public async Task<User> CreateUserAccount(User user)
+    {
+      var Adduser = await _DbContext.User.AddAsync(user);
+      await _DbContext.SaveChangesAsync();
+      return user;
+    }
+
+    //Login 
+    public async Task<User> LoginUser(string email, string password)
+    {
+      var findUser = await _DbContext.User
+        .FirstOrDefaultAsync(u => u.Email == email);
+
+      if (findUser == null)
+        throw new Exception("User not found");
+
+      if (findUser.Password != password)
+        throw new Exception("Wrong password for the given email");
+
+      return findUser;
+
+    }
+
+    //Get by id
+    public async Task<User> GetUserById(Guid Id)
+    {
+      var getUser = await _DbContext.User.FindAsync(Id);
+      if (getUser == null) throw new Exception("user not found");
+      return getUser;
+    }
+  }
+}
---
+++ Backend/Service/UserService.cs
@@ -0,0 +1,37 @@
+﻿using Backend.DTO;
+using Backend.Entity;
+using Backend.Interface;
+
+namespace Backend.Service
+{
+  public class UserService : IUserService
+  {
+    private readonly IUserRepository _iuserRepo;
+    public UserService(IUserRepository userRepository)
+    {
+      _iuserRepo = userRepository;
+    }
+
+    public async Task<User> CreateUserAccount(UserDTO user)
+    {
+      var Mapuser = new User()
+      {
+        Name = user.Name,
+        Email = user.Email,
+        Password = user.Password
+      };
+      var adduser = await _iuserRepo.CreateUserAccount(Mapuser);
+      return adduser;
+    }
+
+    public async Task<User> GetUserById(Guid Id)
+    {
+      return await _iuserRepo.GetUserById(Id);
+    }
+
+    public async Task<User> LoginUser(string email, string password)
+    {
+      return await _iuserRepo.LoginUser(email, password);
+    }
+  }
+}
